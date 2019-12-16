6e60bec6ed08a3e3f32afd0a7ede18565fe2c10f
sarahcmap <sbuchhorn@cmap.illinois.gov>
2019-12-16 16:29:07 -0600

    fill in data for both projects

--- dothings.js
+++ dothings.js
@@ -1,15 +1,47 @@
 var topicselected = [];
 var purposeselected = [];
+var whichproj
 
-$('#1_abm').click(function () {
+$('.card-link').click(function () {
     $('#cardcontainer').hide()
+    whichproj = $(this).attr("id")
     $('#test').show()
-})
+    updateprojpage(whichproj)
+});
 
 $('.gohome').click(function () {
     $('#cardcontainer').show()
     $('#test').hide()
-})
+});
+
+function updateprojpage(whichproj) {
+    var z
+    for (var i = 0; i < dat2.length; i++) {
+        if (dat2[i].selector == whichproj){
+            z = i
+        }
+    } 
+    $('#projname').text(dat2[z].title)
+    $('#topdescription').html("<p style='font-size: 20px;'>" + dat2[z].description + "</p>")
+    $('#firstcol').html("<b>Topic: </b>" + dat2[z].topic +
+        "<hr style='height:10px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Purpose: </b>" + dat2[z].purpose +
+        "<hr style='height:10px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Product: </b>" + dat2[z].product)
+    $('#secondcol').html("<b>Difficulty: </b>" + dat2[z].difficulty +
+        "<hr style='height:10px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Open Source: </b>" + dat2[z].open +
+        "<hr style='height:10px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Interactive: </b>" + dat2[z].interactive)
+    $('#thirdcol').html("<b>Map: </b>" + dat2[z].map +
+        "<hr style='height:10px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Time: </b>" + dat2[z].time)
+    $('#detail2').html("<b>Data: </b>" + dat2[z].data +
+        "<hr style='height:15px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Tools: </b>" + dat2[z].tools +
+        "<hr style='height:15px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Text: </b>" + dat2[z].text +
+        "<hr style='height:15px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Staff: </b>" + dat2[z].staff)
+    $('#documentation').attr('href',dat2[z].projecturl)
+    $('#visitproj').attr('href',dat2[z].docurl)
+    $('#carouselone').attr('src',dat2[z].pic1)
+    $('#carouseltwo').attr('src',dat2[z].pic2)
+    $('#carouselthree').attr('src',dat2[z].pic3)
+};
+
 
 $('#topicfilter').change(function () {
     topicselected = [];
@@ -17,19 +49,22 @@ $('#topicfilter').change(function () {
     $(choices).each(function () {
         topicselected.push($(this).text())
     })
-    if (topicselected.length != 0){
-        topicselected.forEach(element => $('#cardcontainer').find('.card .topic:not(:contains("' + element + '"))').parent().parent().addClass('d-none'))
-    }
-    if (topicselected.length == 0){
+    if (topicselected.length != 0) {
         $('.card').removeClass('d-none')
-        if (purposeselected.length > 0){
+        topicselected.forEach(element => $('#cardcontainer').find('.card .topic:not(:contains("' + element + '"))').parent().parent().addClass('d-none'))
         purposeselected.forEach(element => $('#cardcontainer').find('.card .purpose:not(:contains("' + element + '"))').parent().parent().addClass('d-none'))
-    }
-        else {
-                $('.card').removeClass('d-none');
 
-        }}
-})
+    }
+    if (topicselected.length == 0) {
+        $('.card').removeClass('d-none')
+        if (purposeselected.length > 0) {
+            purposeselected.forEach(element => $('#cardcontainer').find('.card .purpose:not(:contains("' + element + '"))').parent().parent().addClass('d-none'))
+        }
+        else {
+            $('.card').removeClass('d-none');
+        }
+    }
+});
 
 $('#purposefilter').change(function () {
     purposeselected = [];
@@ -37,19 +72,21 @@ $('#purposefilter').change(function () {
     $(choices).each(function () {
         purposeselected.push($(this).text())
     })
-    if (purposeselected.length > 0){
-    purposeselected.forEach(element => $('#cardcontainer').find('.card .purpose:not(:contains("' + element + '"))').parent().parent().addClass('d-none'))
-    }
-    if (purposeselected.length == 0){
+    if (purposeselected.length > 0) {
         $('.card').removeClass('d-none')
-        if (topicselected.length > 0){
+        purposeselected.forEach(element => $('#cardcontainer').find('.card .purpose:not(:contains("' + element + '"))').parent().parent().addClass('d-none'))
+        topicselected.forEach(element => $('#cardcontainer').find('.card .topic:not(:contains("' + element + '"))').parent().parent().addClass('d-none'))
+    }
+    if (purposeselected.length == 0) {
+        $('.card').removeClass('d-none')
+        if (topicselected.length > 0) {
             topicselected.forEach(element => $('#cardcontainer').find('.card .topic:not(:contains("' + element + '"))').parent().parent().addClass('d-none'))
         }
         else {
             $('.card').removeClass('d-none')
         }
     }
-})
+});
 
 
 $(document).ready(function () {
@@ -62,7 +99,7 @@ $(document).ready(function () {
         removeItemButton: true,
     });
 
-    });
+});
 
 
 $.ajax({
@@ -71,19 +108,5 @@ $.ajax({
     dataType: "text",
     success: function (data) {
         dat2 = $.csv.toObjects(data)
-        $('#projname').text(dat2[0].title)
-        $('#topdescription').html("<p style='font-size: 20px;'>" + dat2[0].description + "</p>")
-        $('#firstcol').html("<b>Topic: </b>" + dat2[0].topic +
-            "<hr style='height:10px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Purpose: </b>" + dat2[0].purpose +
-            "<hr style='height:10px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Product: </b>" + dat2[0].product)
-        $('#secondcol').html("<b>Difficulty: </b>" + dat2[0].difficulty +
-            "<hr style='height:10px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Open Source: </b>" + dat2[0].open +
-            "<hr style='height:10px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Interactive: </b>" + dat2[0].interactive)
-        $('#thirdcol').html("<b>Map: </b>" + dat2[0].map +
-            "<hr style='height:10px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Time: </b>" + dat2[0].time)
-        $('#detail2').html("<b>Data: </b>" + dat2[0].data +
-            "<hr style='height:15px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Tools: </b>" + dat2[0].tools +
-            "<hr style='height:15px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Text: </b>" + dat2[0].text +
-            "<hr style='height:15px; margin-top:0px; margin-bottom:0px; visibility:hidden;'/><b>Staff: </b>" + dat2[0].staff)
     }
 });
